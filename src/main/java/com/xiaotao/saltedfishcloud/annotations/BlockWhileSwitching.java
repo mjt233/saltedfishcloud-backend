b5cd5346337aac09a56d93d5cb4d1670b70fa8f9
xiaotao <mjt233@qq.com>
2021-05-16 01:31:25 +0800

    docs 完善BlockWhileSwitching和NotBlock注解的文档注释

--- src/main/java/com/xiaotao/saltedfishcloud/annotations/BlockWhileSwitching.java
+++ src/main/java/com/xiaotao/saltedfishcloud/annotations/BlockWhileSwitching.java
@@ -4,7 +4,9 @@ package com.xiaotao.saltedfishcloud.annotations;
 import java.lang.annotation.*;
 
 /**
- * 当系统处于存储切换状态时阻止执行
+ * 当系统处于存储切换状态时阻止执行<br>
+ * 注解位于类级下时，该控制器类下的所有方法都会被切换中状态所阻塞，除非方法被注解 @NotBlock 所标记
+ * @see NotBlock
  */
 @Documented
 @Target({ElementType.METHOD, ElementType.TYPE})
--- src/main/java/com/xiaotao/saltedfishcloud/annotations/NotBlock.java
+++ src/main/java/com/xiaotao/saltedfishcloud/annotations/NotBlock.java
@@ -2,6 +2,11 @@ package com.xiaotao.saltedfishcloud.annotations;
 
 import java.lang.annotation.*;
 
+/**
+ * 被该注解标记的方法不会被方法所在类的 @BlockWhileSwitching 影响 <br>
+ * 即相当于类级阻塞注解的例外方法<br>
+ * @see BlockWhileSwitching
+ */
 @Documented
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
