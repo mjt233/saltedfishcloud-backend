a6cd5e59176f8b517e2b72c148eaa8eed3bdf48d
xiaotao <mjt233@qq.com>
2021-06-08 23:11:09 +0800

    chore 更新版本号

--- src/main/java/com/xiaotao/saltedfishcloud/config/DiskConfig.java
+++ src/main/java/com/xiaotao/saltedfishcloud/config/DiskConfig.java
@@ -26,6 +26,7 @@ import java.util.Objects;
 @PropertySource("classpath:config.properties")
 @Slf4j
 public class DiskConfig {
+    public static final String VERSION = "1.0.0-SNAPSHOT";
     public static RawPathHandler rawPathHandler;
     public static UniquePathHandler uniquePathHandler;
 
