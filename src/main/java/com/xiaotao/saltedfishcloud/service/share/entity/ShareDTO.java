d3f7376e2db416177160b526f6f9118f3d4c3b90
老咸鱼 <mjt233@qq.com>
2021-11-16 09:45:54 +0800

    docs 更新README.md

--- src/main/java/com/xiaotao/saltedfishcloud/service/share/entity/ShareDTO.java
+++ src/main/java/com/xiaotao/saltedfishcloud/service/share/entity/ShareDTO.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
