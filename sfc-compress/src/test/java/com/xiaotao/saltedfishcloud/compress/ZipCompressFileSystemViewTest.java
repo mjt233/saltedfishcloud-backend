feb5a8ef982b6ce8c905386b37bb79a7cdc66ba8
xiaotao <mjt233@qq.com>
2021-12-12 23:07:04 +0800

    test 增加ZIP压缩解压数据一致性测试

--- sfc-compress/src/test/java/com/xiaotao/saltedfishcloud/compress/ZipCompressFileSystemViewTest.java
+++ sfc-compress/src/test/java/com/xiaotao/saltedfishcloud/compress/ZipCompressFileSystemViewTest.java
@@ -1,9 +1,13 @@
 package com.xiaotao.saltedfishcloud.compress;
 
+import com.xiaotao.saltedfishcloud.compress.creator.ArchiveResourceEntry;
+import com.xiaotao.saltedfishcloud.compress.creator.ZipCompressor;
 import com.xiaotao.saltedfishcloud.compress.reader.impl.ZipArchiveReader;
 import org.apache.commons.compress.archivers.ArchiveException;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.DigestUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,6 +17,33 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 class ZipCompressFileSystemViewTest {
+    private final static ClassPathResource INPUT = new ClassPathResource("/testResources/123.txt");
+    private final static Path OUTPUT = Paths.get("test_output.zip");
+    @Test
+    void compress() throws Exception {
+
+        // 压缩文件
+        ZipCompressor compressor = new ZipCompressor(Files.newOutputStream(OUTPUT));
+        compressor.addFile(new ArchiveResourceEntry("噢/h/h/h/123哈.txt", INPUT.contentLength(), INPUT));
+        compressor.close();
+
+        // 解压文件
+        ZipArchiveReader reader = new ZipArchiveReader(OUTPUT.toFile());
+
+        // 计算原始MD5
+        String originMd5 = DigestUtils.md5DigestAsHex(INPUT.getInputStream());
+
+        // 计算解压后MD5
+        String md5 = DigestUtils.md5DigestAsHex(reader.getInputStream("噢/h/h/h/123哈.txt"));
+
+        // 对比
+        assertEquals(originMd5, md5);
+
+        // 资源释放
+        reader.close();
+        Files.deleteIfExists(OUTPUT);
+    }
+
     @Test
     void extractAll() throws Exception {
         ClassPathResource resource = new ClassPathResource("/testResources/test.zip");
