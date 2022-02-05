0f42f15adca96aecc1a467bce25a05f1b13f0ff7
老咸鱼 <mjt233@qq.com>
2022-02-05 20:34:31 +0800

    feat Hadoop自动配置类设置默认hdfs实现类

--- sfc-ext/sfc-ext-hadoop-store/src/main/java/com/saltedfishcloud/ext/hadoop/HDFSAutoConfigure.java
+++ sfc-ext/sfc-ext-hadoop-store/src/main/java/com/saltedfishcloud/ext/hadoop/HDFSAutoConfigure.java
@@ -25,7 +25,8 @@ public class HDFSAutoConfigure {
     public FileSystem hadoopFileSystem() throws IOException, URISyntaxException, InterruptedException {
         final org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
         conf.set("fs.defaultFS", properties.getUrl());
-        conf.set("hadoop.root.logger", "OFF");
+        conf.set("hadoop.root.logger", "WARN");
+        conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
         return FileSystem.get(new URI(properties.getUrl()), conf, properties.getUser());
     }
 
