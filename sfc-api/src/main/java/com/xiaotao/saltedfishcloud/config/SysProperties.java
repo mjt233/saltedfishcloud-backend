0e45ceac1bb7f3fde665571423734a09f2212d30
老咸鱼 <mjt233@qq.com>
2022-08-29 00:15:02 +0800

    feat 调整FTP服务参数菜单分组

--- sfc-api/src/main/java/com/xiaotao/saltedfishcloud/config/SysProperties.java
+++ sfc-api/src/main/java/com/xiaotao/saltedfishcloud/config/SysProperties.java
@@ -2,6 +2,7 @@ package com.xiaotao.saltedfishcloud.config;
 
 import com.xiaotao.saltedfishcloud.annotations.ConfigProperties;
 import com.xiaotao.saltedfishcloud.annotations.ConfigPropertiesEntity;
+import com.xiaotao.saltedfishcloud.annotations.ConfigPropertiesGroup;
 import com.xiaotao.saltedfishcloud.enums.StoreMode;
 import com.xiaotao.saltedfishcloud.orm.config.annotation.ConfigEntity;
 import com.xiaotao.saltedfishcloud.service.config.SysConfigName;
@@ -117,47 +118,34 @@ public class SysProperties implements InitializingBean {
     }
 
     @Data
-    @ConfigPropertiesEntity
+    @ConfigPropertiesEntity(groups = {
+        @ConfigPropertiesGroup(id = "base", name = "基本信息"),
+        @ConfigPropertiesGroup(id = "passive", name = "被动传输")
+    })
     public static class Ftp {
 
         /**
          * 是否启用FTP服务
          */
-        @ConfigProperties(
-                value = "是否启用",
-                inputType = "checkbox",
-                defaultValue = "true"
-        )
+        @ConfigProperties(value = "是否启用", inputType = "checkbox", defaultValue = "true")
         private boolean ftpEnable = true;
-
 
         /**
          * FTP控制监听地址
          */
-        @ConfigProperties(
-                value = "控制端口监听地址",
-                defaultValue = "21"
-        )
+        @ConfigProperties(value = "控制端口监听地址", defaultValue = "21")
         private String listenAddr = "0.0.0.0";
 
         /**
          * 主控制端口
          */
-        @ConfigProperties(
-                value = "主控制端口",
-                defaultValue = "21",
-                describe = "用于连接控制的端口"
-        )
+        @ConfigProperties(value = "主控制端口", defaultValue = "21", describe = "用于连接控制的端口")
         private int controlPort = 21;
 
         /**
          * 被动传输地址
          */
-        @ConfigProperties(
-                value = "被动传输地址",
-                defaultValue = "localhost",
-                describe = "被动模式下客户端使用连接传输数据的地址"
-        )
+        @ConfigProperties(value = "被动传输地址", defaultValue = "localhost",describe = "被动模式下客户端使用连接传输数据的地址", group = "passive")
         private String passiveAddr = "localhost";
 
         /**
@@ -166,7 +154,8 @@ public class SysProperties implements InitializingBean {
         @ConfigProperties(
                 value = "被动传输端口范围",
                 defaultValue = "20000-30000",
-                describe = "被动模式下服务器开放的数据传输端口范围"
+                describe = "被动模式下服务器开放的数据传输端口范围",
+                group = "passive"
         )
         private String passivePort = "20000-30000";
 
