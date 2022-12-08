a889b4278abd21c0be185734b1d699efbda77366
xiaotao <mjt233@qq.com>
2022-12-09 00:31:12 +0800

    feat 实现ffmpeg字幕提取

--- sfc-ext/sfc-ext-video-enhance/src/main/java/com/saltedfishcloud/ext/ve/core/FFMpegInvoker.java
+++ sfc-ext/sfc-ext-video-enhance/src/main/java/com/saltedfishcloud/ext/ve/core/FFMpegInvoker.java
@@ -3,10 +3,8 @@ package com.saltedfishcloud.ext.ve.core;
 import com.saltedfishcloud.ext.ve.model.VEProperty;
 import org.springframework.util.StreamUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,13 +31,36 @@ public class FFMpegInvoker {
         try (InputStream is = process.getInputStream()) {
             return StreamUtils.copyToString(is, StandardCharsets.UTF_8);
         }
+    }
 
+    /**
+     * 提取视频字幕
+     * @param localFile 本地文件路径
+     * @param streamNo  字幕流编号
+     * @return          字幕文件srt内容
+     */
+    public String extractSubtitle(String localFile, String streamNo) throws IOException {
+        Process process = executeCmd(property.getFFMpegExecPath(), "-i", localFile, "-map", streamNo, "-c", "copy", "-f", "srt", "-loglevel", "error", "-");
+        try (InputStream is = process.getInputStream()) {
+            return StreamUtils.copyToString(is, StandardCharsets.UTF_8);
+        }
     }
 
     /**
      * 执行命令行
      */
     private Process executeCmd(List<String> args) throws IOException {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(args);
+        processBuilder.redirectErrorStream(true);
+        return processBuilder.start();
+    }
+
+    /**
+     * 执行命令行
+     */
+    private Process executeCmd(String... args) throws IOException {
 
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command(args);
--- sfc-ext/sfc-ext-video-enhance/src/test/java/com/saltedfishcloud/ext/vo/test/TestClass.java
+++ sfc-ext/sfc-ext-video-enhance/src/test/java/com/saltedfishcloud/ext/vo/test/TestClass.java
@@ -1,9 +1,11 @@
 package com.saltedfishcloud.ext.vo.test;
 
+import com.saltedfishcloud.ext.ve.core.FFMpegInvoker;
 import com.saltedfishcloud.ext.ve.core.VideoParser;
 import com.saltedfishcloud.ext.ve.model.MediaStream;
 import com.saltedfishcloud.ext.ve.model.VEProperty;
 import com.saltedfishcloud.ext.ve.model.VideoInfo;
+import com.xiaotao.saltedfishcloud.utils.TypeUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,13 +14,14 @@ public class TestClass {
     private final String VIDEO_PATH = "C:\\Users\\xiaotao\\Downloads\\JoJo's Bizarre Adventure - S05E25 - DUAL 1080p WEB H.264 -NanDesuKa (NF).mkv";
     private final String FFMPEG_PATH = "C:\\DATA\\soft\\ffmpeg-master-latest-win64-gpl\\ffmpeg-master-latest-win64-gpl\\bin";
     private final VEProperty PROPERTY = VEProperty.builder().ffmpegPath(FFMPEG_PATH).build();
+    private final FFMpegInvoker invoker = new FFMpegInvoker(PROPERTY);
+    private final VideoParser parser = new VideoParser(PROPERTY);
 
     /**
      * 测试解析mkv视频文件的章节锚点
      */
     @Test
     public void testParseChapter() throws IOException {
-        VideoParser parser = new VideoParser(PROPERTY);
         VideoInfo videoInfo = parser.getVideoInfo(VIDEO_PATH);
         System.out.println("==== 章节锚点 ====");
         System.out.println(videoInfo.getChapterList());
@@ -27,5 +30,17 @@ public class TestClass {
         for (MediaStream stream : videoInfo.getMediaStreamList()) {
             System.out.println(stream.toString());
         }
+    }
+
+    @Test
+    public void testGetSubtitle() throws IOException {
+        VideoInfo videoInfo = parser.getVideoInfo(VIDEO_PATH);
+        MediaStream stream = videoInfo.getMediaStreamList().stream()
+                .filter(e -> e.isSubtitle() && TypeUtils.toNumber(Long.class, e.getMetadata().get("NUMBER_OF_FRAMES")) > 10)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("找不到字幕流"));
+        String srt = invoker.extractSubtitle(VIDEO_PATH, stream.getNo());
+        System.out.println(srt);
+        System.out.println("=== 来自：" + stream);
     }
 }
