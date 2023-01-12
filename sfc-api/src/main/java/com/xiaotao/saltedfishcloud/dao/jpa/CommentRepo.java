e25e3fbdde1f3dfed8a87e5afcf64fe51e1bca69
xiaotao <mjt233@qq.com>
2023-01-13 00:37:33 +0800

    fix 修复Linux下大小敏感导致的数据表找不到问题

--- sfc-api/src/main/java/com/xiaotao/saltedfishcloud/dao/jpa/CommentRepo.java
+++ sfc-api/src/main/java/com/xiaotao/saltedfishcloud/dao/jpa/CommentRepo.java
@@ -15,7 +15,7 @@ public interface CommentRepo extends JpaRepository<Comment, Long> {
      * @param topicId   话题id
      */
     @Query(value = "SELECT a.id, a.uid,a.topic_id,a.content,a.create_at,a.ip,a.is_delete,a.reply_id,a.update_at,u.user as username " +
-            "FROM Comment a " +
+            "FROM comment a " +
             "LEFT JOIN user u ON a.uid = u.id " +
             "WHERE a.topic_id = :topicId ORDER BY a.id DESC", nativeQuery = true)
     List<Comment> findByTopicId(@Param("topicId") Long topicId, Pageable pageable);
