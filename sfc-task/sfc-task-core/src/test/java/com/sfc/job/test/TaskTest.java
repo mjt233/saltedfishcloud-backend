119f245d010c1aca56df4181f2e8a69e07dc316a
xiaotao <mjt233@qq.com>
2023-01-30 21:41:36 +0800

    test RPC测试补充异步并发测试

--- sfc-task/sfc-task-core/src/test/java/com/sfc/job/test/TaskTest.java
+++ sfc-task/sfc-task-core/src/test/java/com/sfc/job/test/TaskTest.java
@@ -1,39 +1,31 @@
 package com.sfc.job.test;
 
-import com.sfc.job.*;
-import com.xiaotao.saltedfishcloud.utils.MapperHolder;
+import com.sfc.job.RPCManager;
+import com.sfc.job.RPCRequest;
+import com.sfc.job.RPCResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.util.Objects;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootTest
@@ -66,12 +58,17 @@ public class TaskTest {
      * @throws IOException
      */
     @Test
-    public void testHandleOnce() throws IOException {
+    public void testHandleOnce() throws IOException, BrokenBarrierException, InterruptedException {
         AtomicInteger aCount = new AtomicInteger();
         AtomicInteger bCount = new AtomicInteger();
         RPCManager rpcManager2 = new RPCManager(factory);
         this.rpcManager.registerRpcHandler("testFunc", request -> {
             boolean isHandled = Integer.parseInt(request.getParam()) % 2 == 0;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             if (isHandled) {
                 aCount.incrementAndGet();
             }
@@ -83,6 +80,11 @@ public class TaskTest {
 
         rpcManager2.registerRpcHandler("testFunc", request -> {
             boolean isHandled = Integer.parseInt(request.getParam()) % 2 != 0;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             if (isHandled) {
                 bCount.incrementAndGet();
             }
@@ -93,17 +95,26 @@ public class TaskTest {
         });
 
 
-        RPCRequest request;
-        RPCResponse<String> response;
-
+        CyclicBarrier barrier = new CyclicBarrier(11);
         for (int i = 0; i < 10; i++) {
-            request = RPCRequest.builder()
-                .functionName("testFunc")
-                .param(i + "")
-                .build();
-            response = this.rpcManager.call(request, String.class);
-            System.out.println("处理结果：" + response.getResult());
+            int j = i;
+            new Thread(() -> {
+                try {
+                    RPCRequest request;
+                    RPCResponse<String> response;
+                    request = RPCRequest.builder()
+                            .functionName("testFunc")
+                            .param(j + "")
+                            .build();
+                    response = this.rpcManager.call(request, String.class);
+                    System.out.println("处理结果：" + response.getResult());
+                    barrier.await();
+                } catch (IOException | InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         }
+        barrier.await();
         System.out.println("a处理次数:" + aCount + " b处理次数:" + bCount);
     }
 }
