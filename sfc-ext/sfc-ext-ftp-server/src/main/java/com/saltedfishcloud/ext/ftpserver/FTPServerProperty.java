fcf7694148608c271d481160a75fb9a5a59e5d96
xiaotao <mjt233@qq.com>
2023-03-20 00:47:45 +0800

    fix 修复FTP服务参数配置对象的配置项名称误用了字段标题的问题

--- sfc-ext/sfc-ext-ftp-server/src/main/java/com/saltedfishcloud/ext/ftpserver/FTPServerProperty.java
+++ sfc-ext/sfc-ext-ftp-server/src/main/java/com/saltedfishcloud/ext/ftpserver/FTPServerProperty.java
@@ -16,32 +16,33 @@ public class FTPServerProperty {
     /**
      * 是否启用FTP服务
      */
-    @ConfigProperties(value = "是否启用", inputType = "switch", defaultValue = "false")
+    @ConfigProperties(value = "ftpEnable", title = "是否启用", inputType = "switch", defaultValue = "false")
     private boolean ftpEnable = false;
 
     /**
      * FTP控制监听地址
      */
-    @ConfigProperties(value = "控制端口监听地址", defaultValue = "0.0.0.0", required = true)
+    @ConfigProperties(value = "listenAddr", title = "控制端口监听地址", defaultValue = "0.0.0.0", required = true)
     private String listenAddr = "0.0.0.0";
 
     /**
      * 主控制端口
      */
-    @ConfigProperties(value = "主控制端口", defaultValue = "2121", describe = "用于连接控制的端口")
+    @ConfigProperties(value = "controlPort", title = "主控制端口", defaultValue = "2121", describe = "用于连接控制的端口")
     private int controlPort = 2121;
 
     /**
      * 被动传输地址
      */
-    @ConfigProperties(value = "被动传输地址", defaultValue = "localhost",describe = "被动模式下客户端使用连接传输数据的地址", group = "passive")
+    @ConfigProperties(value = "passiveAddr", title = "被动传输地址", defaultValue = "localhost",describe = "被动模式下客户端使用连接传输数据的地址", group = "passive")
     private String passiveAddr = "localhost";
 
     /**
      * 被动传输端口范围
      */
     @ConfigProperties(
-            value = "被动传输端口范围",
+            value = "passivePort",
+            title = "被动传输端口范围",
             defaultValue = "20000-30000",
             describe = "被动模式下服务器开放的数据传输端口范围",
             group = "passive"
