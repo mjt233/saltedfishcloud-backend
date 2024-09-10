9a727c36df8d4fa3ba15236d86c146023976ff72
xiaotao <mjt233@qq.com>
2024-09-10 23:43:46 +0800

    feat: Github配置参数补充参数属性注解

--- sfc-core/src/main/java/com/xiaotao/saltedfishcloud/service/third/handler/github/GithubPlatformProperty.java
+++ sfc-core/src/main/java/com/xiaotao/saltedfishcloud/service/third/handler/github/GithubPlatformProperty.java
@@ -1,11 +1,15 @@
 package com.xiaotao.saltedfishcloud.service.third.handler.github;
 
+import com.xiaotao.saltedfishcloud.annotations.ConfigProperty;
 import com.xiaotao.saltedfishcloud.annotations.ConfigPropertyEntity;
 import lombok.Data;
 
 @Data
 @ConfigPropertyEntity
 public class GithubPlatformProperty {
+    @ConfigProperty(required = true, isRow = true, value = "clientId")
     private String clientId;
+
+    @ConfigProperty(required = true, isRow = true, value = "clientSecret")
     private String clientSecret;
 }
