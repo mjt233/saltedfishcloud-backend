54fd8de6334ba23fad022bec676f1d4caee630fc
xiaotao <mjt233@qq.com>
2025-11-19 17:54:04 +0800

    fix: 修复BaseModel的实例在手动赋值id后，与保存到数据库时实际使用的id不一致的问题

--- sfc-api/src/main/java/com/xiaotao/saltedfishcloud/utils/identifier/SnowFlakeIdGenerator.java
+++ sfc-api/src/main/java/com/xiaotao/saltedfishcloud/utils/identifier/SnowFlakeIdGenerator.java
@@ -1,5 +1,6 @@
 package com.xiaotao.saltedfishcloud.utils.identifier;
 
+import com.xiaotao.saltedfishcloud.model.template.BaseModel;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
@@ -8,6 +9,13 @@ import java.io.Serializable;
 public class SnowFlakeIdGenerator implements IdentifierGenerator {
     @Override
     public Serializable generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) {
+        if (o instanceof BaseModel model) {
+            if (model.getId() == null) {
+                return IdUtil.getId();
+            } else {
+                return model.getId();
+            }
+        }
         return IdUtil.getId();
     }
 }
