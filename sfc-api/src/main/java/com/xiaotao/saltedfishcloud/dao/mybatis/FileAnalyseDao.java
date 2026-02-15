0212551c88faba40be7f15a0a1cd2d33b8ccbd57
xiaotao <mjt233@qq.com>
2026-02-15 12:10:23 +0800

    perf: 优化WebDAV处理PROPFIND操作时，数据库CPU吃到100%且接口响应极慢的问题

--- sfc-api/src/main/java/com/xiaotao/saltedfishcloud/dao/mybatis/FileAnalyseDao.java
+++ sfc-api/src/main/java/com/xiaotao/saltedfishcloud/dao/mybatis/FileAnalyseDao.java
@@ -9,57 +9,36 @@ public interface FileAnalyseDao {
     /**
      * 取用户数据总大小
      */
-    @Select("SELECT SUM(size) FROM file_table WHERE size != -1 AND uid != 0")
+    @Select("SELECT SUM(size) FROM file_table WHERE size != -1 AND uid != 0 AND (is_mount IS NULL OR is_mount = 0)")
     Long getUserTotalSize();
 
     /**
      * 取用户数据总大小
      */
-    @Select("SELECT SUM(size) FROM file_table WHERE size != -1 AND uid = 0")
+    @Select("SELECT SUM(size) FROM file_table WHERE size != -1 AND uid = 0 AND (is_mount IS NULL OR is_mount = 0)")
     Long getPublicTotalSize();
 
     /**
-     * 取唯一存储下用户实际数据大小
-     */
-    @Select("SELECT SUM(size) FROM (SELECT ANY_VALUE(size) size FROM file_table WHERE size != -1 AND uid != 0 GROUP BY md5) AS temp")
-    Long getRealTotalUserSize();
-
-    /**
      * 取目录数
      */
-    @Select("SELECT count(*) FROM file_table WHERE size = -1")
-    Long getDirCount();
-
-    /**
-     * 取目录数
-     */
-    @Select("SELECT count(*) FROM file_table WHERE uid = 0 AND size = -1")
+    @Select("SELECT count(*) FROM file_table WHERE uid = 0 AND size = -1 AND (is_mount IS NULL OR is_mount = 0)")
     Long getPublicDirCount();
 
     /**
      * 取目录数
      */
-    @Select("SELECT count(*) FROM file_table WHERE uid != 0 AND size = -1")
+    @Select("SELECT count(*) FROM file_table WHERE uid != 0 AND size = -1 AND (is_mount IS NULL OR is_mount = 0)")
     Long getUserDirCount();
 
     /**
-     * 取文件数
-     */
-    @Select("SELECT count(*) FROM file_table WHERE size != -1")
-    Long getFileCount();
-
-    /**
      * 取公共网盘文件数
      */
-    @Select("SELECT count(*) FROM file_table WHERE uid = 0 AND size != -1 ")
+    @Select("SELECT count(*) FROM file_table WHERE uid = 0 AND size != -1 AND (is_mount IS NULL OR is_mount = 0)")
     Long getPublicFileCount();
 
     /**
-     * 取公共网盘文件数
+     * 取私人网盘文件数
      */
-    @Select("SELECT count(*) FROM file_table WHERE uid != 0 && size != -1 ")
+    @Select("SELECT count(*) FROM file_table WHERE uid != 0 AND size != -1 AND (is_mount IS NULL OR is_mount = 0)")
     Long getUserFileCount();
-
-    @Select("SELECT SUM(size) FROM file_table WHERE size != -1 AND uid = 123123")
-    Long test();
 }
