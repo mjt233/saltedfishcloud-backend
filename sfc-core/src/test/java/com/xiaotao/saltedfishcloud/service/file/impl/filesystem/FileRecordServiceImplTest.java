86b802006f68772ead43a75bb725a9b65104afce
xiaotao <mjt233@qq.com>
2026-02-18 16:55:13 +0800

    test: 封装MockFileSystem

--- sfc-core/src/test/java/com/xiaotao/saltedfishcloud/service/file/impl/filesystem/FileRecordServiceImplTest.java
+++ sfc-core/src/test/java/com/xiaotao/saltedfishcloud/service/file/impl/filesystem/FileRecordServiceImplTest.java
@@ -2,8 +2,8 @@ package com.xiaotao.saltedfishcloud.service.file.impl.filesystem;
 
 import com.xiaotao.saltedfishcloud.dao.jpa.FileInfoRepo;
 import com.xiaotao.saltedfishcloud.model.po.file.FileInfo;
+import com.xiaotao.saltedfishcloud.test.MockFileSystem;
 import com.xiaotao.saltedfishcloud.utils.SecureUtils;
-import com.xiaotao.saltedfishcloud.utils.identifier.IdUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,9 +12,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.*;
-import java.util.function.Consumer;
+import java.util.Deque;
+import java.util.List;
+import java.util.Optional;
 
+import static com.xiaotao.saltedfishcloud.test.MockFileSystem.createDir;
+import static com.xiaotao.saltedfishcloud.test.MockFileSystem.createFile;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,57 +28,9 @@ class FileRecordServiceImplTest {
     @InjectMocks
     private FileRecordServiceImpl fileRecordService;
 
+    private final MockFileSystem mockFileSystem = new MockFileSystem();
+
     static String ROOT_ID = "0";
-
-    static Map<String, List<FileInfo>> VIRTUAL_DIR_PARENT_MAP = new HashMap<>();
-    static Map<String, FileInfo> VIRTUAL_DIR_MAP = new HashMap<>();
-    static Map<String, String> NAME_ID_MAP = new HashMap<>();
-
-    /**
-     * 向模拟文件系统数据集中添加一个文件或目录
-     *
-     * @param fileInfo 待添加的文件或目录
-     */
-    private static MockFileSystemHandler registerMockFile(FileInfo fileInfo) {
-        VIRTUAL_DIR_PARENT_MAP
-                .computeIfAbsent(fileInfo.getNode(), k -> new ArrayList<>())
-                .add(fileInfo);
-        if (fileInfo.isDir()) {
-            NAME_ID_MAP.put(fileInfo.getName(), fileInfo.getMd5());
-            VIRTUAL_DIR_MAP.put(fileInfo.getMd5(), fileInfo);
-        }
-        return new MockFileSystemHandler(fileInfo);
-    }
-
-    public record MockFileSystemHandler(FileInfo parent) {
-
-        public MockFileSystemHandler addSubFile(String name) {
-            return registerMockFile(createFile(name, parent.getMd5()));
-        }
-
-        public MockFileSystemHandler addSubDir(String name) {
-            return registerMockFile(createDir(name, parent.getMd5()));
-        }
-
-        public void andThen(Consumer<MockFileSystemHandler> consumer) {
-            consumer.accept(this);
-        }
-    }
-
-    private static FileInfo createDir(String name, String parent) {
-        FileInfo f = new FileInfo().setName(name).setMd5(SecureUtils.getUUID()).setNode(parent);
-        f.setSize(-1L);
-        f.setUid(0L);
-        f.setId(IdUtil.getId());
-        return f;
-    }
-
-
-
-    private static FileInfo createFile(String name, String parent) {
-        return createDir(name, parent).setSize(114514L);
-    }
-
 
 
     /**
@@ -107,8 +62,8 @@ class FileRecordServiceImplTest {
      *         └── a3b2c2
      * </pre>
      */
-    static {
-        registerMockFile(createDir("a1", ROOT_ID)).andThen(a1 -> {
+    {
+        mockFileSystem.registerMockFile(createDir("a1", ROOT_ID)).andThen(a1 -> {
             a1.addSubDir("a1b1").andThen(a1b1 -> {
                 a1b1.addSubDir("a1b1c1");
                 a1b1.addSubDir("a1b1c2");
@@ -120,7 +75,7 @@ class FileRecordServiceImplTest {
         });
 
 
-        registerMockFile(createDir("a2", ROOT_ID)).andThen(a2 -> {
+        mockFileSystem.registerMockFile(createDir("a2", ROOT_ID)).andThen(a2 -> {
             a2.addSubDir("a2b1").andThen(a2b1 -> {
                 a2b1.addSubDir("a2b1c1");
                 a2b1.addSubDir("a2b1c2");
@@ -131,7 +86,7 @@ class FileRecordServiceImplTest {
             });
         });
 
-        registerMockFile(createDir("a3", ROOT_ID)).andThen(a3 -> {
+        mockFileSystem.registerMockFile(createDir("a3", ROOT_ID)).andThen(a3 -> {
             a3.addSubDir("a3b1").andThen(a3b1 -> {
                 a3b1.addSubDir("a3b1c1").andThen(a3b1c1 -> {
                     a3b1c1.addSubDir("a3b1c1d1");
@@ -153,10 +108,15 @@ class FileRecordServiceImplTest {
         Mockito.when(fileInfoRepo.findDirByUidAndNode(Mockito.anyLong(), Mockito.anyString()))
                 .thenAnswer(i -> {
                     String node = i.getArgument(1, String.class);
-                    return VIRTUAL_DIR_PARENT_MAP.get(node);
+                    return Optional.ofNullable(mockFileSystem.listFiles(node))
+                            .map(e -> e.stream()
+                                    .filter(FileInfo::isDir)
+                                    .toList()
+                            )
+                            .orElse(null);
                 });
 
-        String a3Id = NAME_ID_MAP.get("a3");
+        String a3Id = mockFileSystem.getMd5ByName("a3");
         assertEquals(8, fileRecordService.listChildDirs(0, a3Id, -1).size());
         assertEquals(2, fileRecordService.listChildDirs(0, a3Id, 0).size());
         List<FileInfo> r = fileRecordService.listChildDirs(0, a3Id, 1);
@@ -172,15 +132,15 @@ class FileRecordServiceImplTest {
         Mockito.when(fileInfoRepo.findDirByUidAndMd5(Mockito.anyLong(), Mockito.anyString()))
                 .thenAnswer(i -> {
                     String nodeId = i.getArgument(1, String.class);
-                    return VIRTUAL_DIR_MAP.get(nodeId);
+                    return mockFileSystem.getDirFileInfoByMd5(nodeId);
                 });
-        Optional<Deque<FileInfo>> res = fileRecordService.listAllParentByNodeId(0, NAME_ID_MAP.get("a1b1c1"));
+        Optional<Deque<FileInfo>> res = fileRecordService.listAllParentByNodeId(0, mockFileSystem.getMd5ByName("a1b1c1"));
         assertTrue(res.isPresent());
         Deque<FileInfo> parentList = res.get();
         assertEquals(3, parentList.size());
         assertEquals("a1", parentList.getFirst().getName());
         assertEquals("a1b1c1", parentList.getLast().getName());
-        assertFalse(fileRecordService.listAllParentByNodeId(0, NAME_ID_MAP.get("a1b1c11")).isPresent());
+        assertFalse(fileRecordService.listAllParentByNodeId(0, mockFileSystem.getMd5ByName("a1b1c11")).isPresent());
         assertFalse(fileRecordService.listAllParentByNodeId(0, SecureUtils.getUUID()).isPresent());
     }
 
@@ -191,7 +151,7 @@ class FileRecordServiceImplTest {
                 .thenAnswer(i -> {
                     String name = i.getArgument(1, String.class);
                     String parentId = i.getArgument(2, String.class);
-                    List<FileInfo> fileInfos = VIRTUAL_DIR_PARENT_MAP.get(parentId);
+                    List<FileInfo> fileInfos = mockFileSystem.listFiles(parentId);
                     if (fileInfos == null) {
                         return null;
                     }
@@ -220,7 +180,7 @@ class FileRecordServiceImplTest {
         assertEquals("/", rootResult.getFirst().getPath());
 
         // 路径中间混入文件类型节点
-        registerMockFile(createFile("a3b1File", NAME_ID_MAP.get("a3b1")));
+        mockFileSystem.registerMockFile(createFile("a3b1File", mockFileSystem.getMd5ByName("a3b1")));
         assertNull(fileRecordService.getVisitPathInfo(0, "/a3/a3b1/a3b1File/a3b1File111"));
     }
 }
