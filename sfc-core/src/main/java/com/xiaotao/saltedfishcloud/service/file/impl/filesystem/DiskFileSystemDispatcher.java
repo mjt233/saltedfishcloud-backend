c6726993d644645cc572fc95a90d0e0d3740c64a
xiaotao <mjt233@qq.com>
2026-04-15 21:41:19 +0800

    refactor: 移动FileSystemMatchResult类到新文件，优化代码结构

--- sfc-core/src/main/java/com/xiaotao/saltedfishcloud/service/file/impl/filesystem/DiskFileSystemDispatcher.java
+++ sfc-core/src/main/java/com/xiaotao/saltedfishcloud/service/file/impl/filesystem/DiskFileSystemDispatcher.java
@@ -44,57 +44,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
- * 文件系统匹配结果
- */
-class FileSystemMatchResult {
-    /**
-     * 匹配到的文件系统
-     */
-    DiskFileSystem fileSystem;
-
-    /**
-     * 在对应的文件系统上解析后的对应路径（原始请求路径）
-     */
-    String resolvedPath;
-
-    /**
-     * 匹配到的挂载点，若没匹配到则为null
-     */
-    MountPoint mountPoint;
-
-    public FileSystemMatchResult(DiskFileSystem fileSystem,MountPoint mountPoint, String resolvedPath) {
-        this.fileSystem = fileSystem;
-        this.mountPoint = mountPoint;
-        this.resolvedPath = resolvedPath;
-    }
-
-    @Override
-    public String toString() {
-        return "FileSystemMatchResult{" +
-                "fileSystem=" + fileSystem +
-                ", resolvedPath='" + resolvedPath + '\'' +
-                '}';
-    }
-
-    /**
-     * 判断请求的路径是否为匹配的挂载点本身
-     */
-    public boolean isMountPath(String path) {
-        if (mountPoint == null) {
-            return false;
-        }
-        return StringUtils.isPathEqual(path, mountPoint.getPath());
-    }
-
-    /**
-     * 判断是否为一个启用了代理文件存储记录的挂载点
-     */
-    public boolean isProxyStoreRecordMountPoint() {
-        return mountPoint != null && Boolean.TRUE.equals(mountPoint.getIsProxyStoreRecord());
-    }
-}
-
-/**
  * 特殊的文件系统，文件系统指派器，根据请求的路径指派相对应的文件系统类型来执行相关请求，为实现第三方文件系统挂载点功能提供支持。
  */
 @Slf4j
