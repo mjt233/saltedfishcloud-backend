c6492a1924c6bf07192f73ced7b17898558c68f2
xiaotao <mjt233@qq.com>
2026-05-19 00:48:58 +0800

    feat: 修改创建临时下载链接接口，支持从请求体中接收资源请求参数

--- sfc-core/src/main/java/com/xiaotao/saltedfishcloud/controller/FileLinkController.java
+++ sfc-core/src/main/java/com/xiaotao/saltedfishcloud/controller/FileLinkController.java
@@ -17,11 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -53,7 +49,7 @@ public class FileLinkController {
      */
     @ApiOperation("创建临时下载链接")
     @PostMapping("/create")
-    public JsonResult<String> createLink(@RequestParam("baseUrl") String baseUrl, @Valid ResourceRequest resourceRequest) throws IOException {
+    public JsonResult<String> createLink(@RequestParam("baseUrl") String baseUrl,@RequestBody @Valid ResourceRequest resourceRequest) throws IOException {
         Resource resource;
         try {
             resource = resourceService.getResource(resourceRequest);
