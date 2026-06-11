fef0552cc68591c4821de2599b87d34a7ece185d
xiaotao <mjt233@qq.com>
2026-06-11 11:29:11 +0800

    feat: 更新数据管理插件配置，添加对其他插件的依赖，方便后续根据其他插件的加载情况动态获得对应的识别能力

--- sfc-ext/sfc-ext-data-manager/src/main/java/com/sfc/dm/config/DataManagerAutoConfiguration.java
+++ sfc-ext/sfc-ext-data-manager/src/main/java/com/sfc/dm/config/DataManagerAutoConfiguration.java
@@ -1,33 +1,8 @@
 package com.sfc.dm.config;
 
-import com.sfc.dm.controller.DataManagerController;
-import com.sfc.dm.service.DataExportService;
-import com.sfc.dm.service.DataImportService;
-import com.sfc.dm.service.FileMetadataBackupService;
-import org.springframework.context.annotation.Bean;
-
 /**
  * 数据管理插件自动配置
  */
 public class DataManagerAutoConfiguration {
 
-    @Bean
-    public DataManagerController dataManagerController() {
-        return new DataManagerController();
-    }
-
-    @Bean
-    public DataExportService dataExportService() {
-        return new DataExportService();
-    }
-
-    @Bean
-    public DataImportService dataImportService() {
-        return new DataImportService();
-    }
-
-    @Bean
-    public FileMetadataBackupService fileMetadataBackupService() {
-        return new FileMetadataBackupService();
-    }
 }
