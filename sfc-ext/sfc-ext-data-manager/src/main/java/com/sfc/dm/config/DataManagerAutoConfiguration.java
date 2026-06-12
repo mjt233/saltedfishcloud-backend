44635ee89108ec720c94d4d43c0fa4a4c3add0b1
xiaotao <mjt233@qq.com>
2026-06-12 17:50:55 +0800

    fix(data-manager): 修复缺少FFMpegHelper时系统无法启动的问题

--- sfc-ext/sfc-ext-data-manager/src/main/java/com/sfc/dm/config/DataManagerAutoConfiguration.java
+++ sfc-ext/sfc-ext-data-manager/src/main/java/com/sfc/dm/config/DataManagerAutoConfiguration.java
@@ -5,17 +5,15 @@ import com.sfc.dm.model.po.InvalidDataRecord;
 import com.sfc.dm.repo.InvalidDataRecordRepo;
 import com.sfc.dm.service.ClaimService;
 import com.sfc.dm.service.InvalidDataService;
-import com.sfc.dm.service.identify.FileTypeChecker;
 import com.sfc.dm.service.identify.FileTypeCheckerImpl;
 import com.sfc.dm.service.identify.AudioCheckProvider;
 import com.sfc.dm.service.identify.VideoCheckProvider;
-import com.sfc.task.AsyncTaskManager;
-import com.sfc.task.repo.AsyncTaskRecordRepo;
 import com.saltedfishcloud.ext.ve.core.FFMpegHelper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -24,46 +22,31 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EntityScan(basePackageClasses = {InvalidDataRecord.class})
 @EnableJpaRepositories(basePackageClasses = {InvalidDataRecordRepo.class})
+@Import({
+        InvalidDataController.class,
+        InvalidDataService.class,
+        ClaimService.class,
+        FileTypeCheckerImpl.class
+})
 public class DataManagerAutoConfiguration {
 
-    @Bean
-    public InvalidDataController invalidDataController(InvalidDataService invalidDataService,
-                                                        ClaimService claimService,
-                                                        AsyncTaskManager asyncTaskManager,
-                                                        AsyncTaskRecordRepo asyncTaskRecordRepo) {
-        return new InvalidDataController(invalidDataService, claimService, asyncTaskManager, asyncTaskRecordRepo);
-    }
-
-    @Bean
-    public InvalidDataService invalidDataService() {
-        return new InvalidDataService();
-    }
-
-    @Bean
-    public ClaimService claimService() {
-        return new ClaimService();
-    }
-
-    @Bean
-    public FileTypeChecker fileTypeChecker() {
-        return new FileTypeCheckerImpl();
-    }
-
-    /**
-     * 当sfc-ext-video-enhance插件存在时，注册视频文件类型识别提供者
-     */
-    @Bean
+    @Configuration
     @ConditionalOnClass(name = "com.saltedfishcloud.ext.ve.core.FFMpegHelper")
-    public VideoCheckProvider videoCheckProvider(FFMpegHelper ffMpegHelper) {
-        return new VideoCheckProvider(ffMpegHelper);
-    }
+    public static class FFMpegProviderConfiguration {
+        /**
+         * 当sfc-ext-video-enhance插件存在时，注册视频文件类型识别提供者
+         */
+        @Bean
+        public VideoCheckProvider videoCheckProvider(FFMpegHelper ffMpegHelper) {
+            return new VideoCheckProvider(ffMpegHelper);
+        }
 
-    /**
-     * 当sfc-ext-video-enhance插件存在时，注册音频文件类型识别提供者
-     */
-    @Bean
-    @ConditionalOnClass(name = "com.saltedfishcloud.ext.ve.core.FFMpegHelper")
-    public AudioCheckProvider audioCheckProvider(FFMpegHelper ffMpegHelper) {
-        return new AudioCheckProvider(ffMpegHelper);
+        /**
+         * 当sfc-ext-video-enhance插件存在时，注册音频文件类型识别提供者
+         */
+        @Bean
+        public AudioCheckProvider audioCheckProvider(FFMpegHelper ffMpegHelper) {
+            return new AudioCheckProvider(ffMpegHelper);
+        }
     }
 }
